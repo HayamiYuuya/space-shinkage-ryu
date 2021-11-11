64442a973cdd6cd5958878b2a5fc781fffb25bd3
早見勇弥 <40552570+HayamiYuuya@users.noreply.github.com>
2021-11-11 16:45:18 +0800

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,3 @@
-import Head from 'next/head'
-import styles from './layout.module.css'
-
 export default function Layout({ children }) {
-  return (
-    <>
-      <Head>
-        <title>Layouts Example</title>
-      </Head>
-      <main className={styles.main}>{children}</main>
-    </>
-  )
+  return <div>{children}</div>
 }

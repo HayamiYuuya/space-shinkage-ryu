ba39d575c117ea4a9a8d38175757f4c49cc0513a
早見勇弥 <40552570+HayamiYuuya@users.noreply.github.com>
2021-11-14 05:51:44 +0800

    Update index.js

--- pages/support/index.js
+++ pages/support/index.js
@@ -1,6 +1,7 @@
 //space-shinkage-ryu.com/support/
 import Head from 'next/head'
 import Link from 'next/link'
+omport Layout from '../../components/layout/layout'
 
 function SupportPage () {
   return ( 
@@ -8,10 +9,12 @@ function SupportPage () {
     <Head>
       <title>Space Shinkage-ryu | Support</title>
     </Head>
+    <Layout>
     <h1>Support Page</h1>
     <Link href="/">
       <a>go back</a>
     </Link>
+    </Layout>
     </>
     )
 }

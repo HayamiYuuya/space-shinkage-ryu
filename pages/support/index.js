6b1c26847342cd525b581d6b10678da6423124f6
早見勇弥 <40552570+HayamiYuuya@users.noreply.github.com>
2021-11-14 06:21:45 +0800

    Update index.js

--- pages/support/index.js
+++ pages/support/index.js
@@ -4,12 +4,10 @@ import Link from 'next/link'
 
 function SupportPage () {
   return ( 
-    <>
     <h1>Support Page</h1>
     <Link href="/">
       <a>go back</a>
     </Link>
-    </>
     )
 }
 
